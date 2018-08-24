4952faf4302d4c05f38b77f4d33196f8befafb83
Cameron Aziz <cameronaziz@me.com>
2018-08-24 06:10:52 -0700

    reorder

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,22 +5,22 @@ import { Content, Inner, Title, ProjectsWrapper } from './styled';
 
 const projects = [
   {
+    title: 'Precision Filter',
+    bg: 'linear-gradient(to right, #d4145a 0%, #8e14d4 100%)',
+    description: 'Engineer internal tool to recieve product pricing from vendors, place orders and track inventory.',
+    tech: ['nodejs', 'react', 'graphql', 'firebase'],
+  },
+  {
     title: 'Any Game Tickets',
-    bg: 'linear-gradient(to right, #009245 0%, #FCEE21 100%)',
+    bg: 'linear-gradient(to right, #145ad4 0%, #bad414 100%)',
     description: 'Develop ticking application by interfacing with APIs and rendering dynamic D3 SVGs.',
     tech: ['nodejs', 'react', 'mongo', 'graphql', 'd3'],
   },
   {
     title: 'Patient Pop',
-    bg: 'linear-gradient(to right, #145ad4 0%, #bad414 100%)',
+    bg: 'linear-gradient(to right, #009245 0%, #FCEE21 100%)',
     description: 'Design and engineer application to automate onboarding of new customers into AWS infrastructure.',
     tech: ['php', 'awscm', 'laravel'],
-  },
-  {
-    title: 'Precision Filter',
-    bg: 'linear-gradient(to right, #d4145a 0%, #8e14d4 100%)',
-    description: 'Engineer internal tool to recieve product pricing from vendors, place orders and track inventory.',
-    tech: ['nodejs', 'react', 'graphql', 'firebase'],
   },
   {
     title: 'Elevare',
