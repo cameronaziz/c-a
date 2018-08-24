7ade3cdd131a5941e29e1736541d53bb340df00d
Cameron Aziz <cameronaziz@me.com>
2018-08-24 06:08:36 -0700

    text

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,23 +1,127 @@
-import React, { Fragment } from 'react';
-import PropTypes from 'prop-types';
-import ReactTooltip from 'react-tooltip';
+import React, { Component } from 'react';
+import PropTypes, { element } from 'prop-types';
+import d3SVG from '../images/d3.svg';
+import reactSVG from '../images/react.svg';
+import graphQLSVG from '../images/graphQL.svg';
+import php from '../images/php.svg';
+import awscm from '../images/awscm.svg';
+import mongo from '../images/mongo.svg';
+import firebase from '../images/firebase.svg';
+import laravel from '../images/laravel.svg';
+import nodejs from '../images/nodejs.svg';
 
 import { Wrapper, Text, CardTitle } from './styled';
 
-const ProjectCard = ({ title, link, children, bg, tech }) => (
-  <Wrapper href={link} rel="noopener noreferrer" bg={bg}>
-    <Text>{children}</Text>
-    <CardTitle>{title}</CardTitle>
-    {tech &&
-      tech.map(item => (
-        <Fragment>
-          <img data-tip={item.name} src={item.icon} width="50px" height="50px" alt={item.name} />
-          <ReactTooltip place="top" type="dark" effect="float" />
-        </Fragment>
-      ))}
-  </Wrapper>
-);
+const libaries = [
+  {
+    name: 'react',
+    title: 'React',
+    icon: reactSVG,
+    text: 'React is a JavaScript library for building user interfaces developed by Facebook.',
+  },
+  {
+    name: 'graphql',
+    title: 'GraphQL',
+    icon: graphQLSVG,
+    text: 'GraphQL is a query language for APIs and a runtime for fulfilling those queries with your existing data.',
+  },
+  {
+    name: 'mongo',
+    title: 'MongoDB',
+    icon: mongo,
+    text:
+      'MongoDB is a document database with the scalability and flexibility with a data structure can be changed over time',
+  },
+  {
+    name: 'd3',
+    title: 'D3',
+    icon: d3SVG,
+    text: 'D3.js is a JavaScript library for manipulating documents based on data.',
+  },
+  {
+    name: 'php',
+    title: 'PHP',
+    icon: php,
+    text: 'PHP is a popular general-purpose scripting language that is especially suited to web development.',
+  },
+  {
+    name: 'awscm',
+    title: 'AWS Certificate Manager',
+    icon: awscm,
+    text:
+      'AWS Certificate Manager is a service that lets you easily provision, manage, and deploy public and private SSL and TLS certificates',
+  },
+  {
+    name: 'laravel',
+    title: 'Laravel',
+    icon: laravel,
+    text: 'Laravel is a web application framework with expressive, elegant syntax.',
+  },
+  // {
+  //   name: 'javascript',
+  //   title: 'Javascript',
+  //   icon: javascript,
+  //   text: 'JavaScript is a scripting or programming language that allows you to implement complex things on web pages.',
+  // },
+  {
+    name: 'nodejs',
+    title: 'NodeJS',
+    icon: nodejs,
+    text: "Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine.",
+  },
+  {
+    name: 'firebase',
+    title: 'Firebase',
+    icon: firebase,
+    text:
+      "Firebase is Google's mobile platform that helps you quickly develop high-quality apps and grow your business.",
+  },
+];
 
+class ProjectCard extends Component {
+  state = { libaryText: '' };
+
+  hoverItem = item => {
+    const libaryText = item.text || '';
+    this.setState({
+      libaryText,
+    });
+  };
+
+  leaveItem = () => {
+    this.setState({
+      libaryText: '',
+    });
+  };
+
+  render() {
+    const { libaryText } = this.state;
+    const { link, children, title, bg, tech } = this.props;
+    return (
+      <Wrapper href={link} rel="noopener noreferrer" bg={bg}>
+        <Text>{children}</Text>
+        <CardTitle>{title}</CardTitle>
+        {tech &&
+          tech.map(item => {
+            const libary = libaries.find(e => e.name === item);
+            return (
+              <span
+                onMouseEnter={() => {
+                  this.hoverItem(libary);
+                }}
+                onMouseLeave={this.leaveItem}
+              >
+                <img src={libary.icon} width="50px" height="50px" alt={libary.name} />
+              </span>
+            );
+          })}
+        <div style={{ height: '18px' }}>
+          <Text>{libaryText}</Text>
+        </div>
+      </Wrapper>
+    );
+  }
+}
 export default ProjectCard;
 
 ProjectCard.propTypes = {
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,100 +2,32 @@ import React from 'react';
 
 import ProjectCard from './ProjectCard';
 import { Content, Inner, Title, ProjectsWrapper } from './styled';
-import d3SVG from '../images/d3.svg';
-import reactSVG from '../images/react.svg';
-import graphQLSVG from '../images/graphQL.svg';
-import php from '../images/php.svg';
-import awscm from '../images/awscm.svg';
-import mongo from '../images/mongo.svg';
-import firebase from '../images/firebase.svg';
-import laravel from '../images/laravel.svg';
 
 const projects = [
-  {
-    title: 'Elevare',
-    bg: 'linear-gradient(to right, #D4145A 0%, #FBB03B 100%)',
-    description:
-      'Architect application to request data from APIs such as Facebook Graph, Google Analytics and Yelp Fusion.',
-    tech: [
-      {
-        icon: reactSVG,
-        name: 'React',
-      },
-      {
-        icon: graphQLSVG,
-        name: 'GraphQL',
-      },
-      {
-        icon: mongo,
-        name: 'MongoDB',
-      },
-      {
-        icon: d3SVG,
-        name: 'D3',
-      },
-    ],
-  },
   {
     title: 'Any Game Tickets',
     bg: 'linear-gradient(to right, #009245 0%, #FCEE21 100%)',
     description: 'Develop ticking application by interfacing with APIs and rendering dynamic D3 SVGs.',
-    tech: [
-      {
-        icon: reactSVG,
-        name: 'React',
-      },
-      {
-        icon: graphQLSVG,
-        name: 'GraphQL',
-      },
-      {
-        icon: mongo,
-        name: 'MongoDB',
-      },
-      {
-        icon: d3SVG,
-        name: 'D3',
-      },
-    ],
+    tech: ['nodejs', 'react', 'mongo', 'graphql', 'd3'],
   },
   {
     title: 'Patient Pop',
-    bg: 'linear-gradient(to right, #662D8C 0%, #ED1E79 100%)',
+    bg: 'linear-gradient(to right, #145ad4 0%, #bad414 100%)',
     description: 'Design and engineer application to automate onboarding of new customers into AWS infrastructure.',
-    tech: [
-      {
-        icon: php,
-        name: 'PHP',
-      },
-      {
-        icon: awscm,
-        name: 'AWS Certificate Manager',
-      },
-      {
-        icon: laravel,
-        name: 'Laravel',
-      },
-    ],
+    tech: ['php', 'awscm', 'laravel'],
   },
   {
     title: 'Precision Filter',
-    bg: 'linear-gradient(to right, #D585FF 0%, #00FFEE 100%)',
+    bg: 'linear-gradient(to right, #d4145a 0%, #8e14d4 100%)',
     description: 'Engineer internal tool to recieve product pricing from vendors, place orders and track inventory.',
-    tech: [
-      {
-        icon: reactSVG,
-        name: 'React',
-      },
-      {
-        icon: graphQLSVG,
-        name: 'GraphQL',
-      },
-      {
-        icon: firebase,
-        name: 'Firebase',
-      },
-    ],
+    tech: ['nodejs', 'react', 'graphql', 'firebase'],
+  },
+  {
+    title: 'Elevare',
+    bg: 'linear-gradient(to right, #d48e14 0%, #d42e14 100%)',
+    description:
+      'Architect application to request data from APIs such as Facebook Graph, Google Analytics and Yelp Fusion.',
+    tech: ['nodejs', 'react', 'mongo', 'graphql', 'd3'],
   },
 ];
 
