2b1b36d428caa25497211202f8763d801c94dd33
Cameron Aziz <cameronaziz@me.com>
2018-08-24 06:16:11 -0700

    reorder

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,22 +5,16 @@ import { Content, Inner, Title, ProjectsWrapper } from './styled';
 
 const projects = [
   {
-    title: 'Precision Filter',
-    bg: 'linear-gradient(to right, #d4145a 0%, #8e14d4 100%)',
-    description: 'Engineer internal tool to recieve product pricing from vendors, place orders and track inventory.',
-    tech: ['nodejs', 'react', 'graphql', 'firebase'],
-  },
-  {
     title: 'Any Game Tickets',
     bg: 'linear-gradient(to right, #145ad4 0%, #bad414 100%)',
     description: 'Develop ticking application by interfacing with APIs and rendering dynamic D3 SVGs.',
     tech: ['nodejs', 'react', 'mongo', 'graphql', 'd3'],
   },
   {
-    title: 'Patient Pop',
-    bg: 'linear-gradient(to right, #009245 0%, #FCEE21 100%)',
-    description: 'Design and engineer application to automate onboarding of new customers into AWS infrastructure.',
-    tech: ['php', 'awscm', 'laravel'],
+    title: 'Precision Filter',
+    bg: 'linear-gradient(to right, #d4145a 0%, #8e14d4 100%)',
+    description: 'Engineer internal tool to recieve product pricing from vendors, place orders and track inventory.',
+    tech: ['nodejs', 'react', 'graphql', 'firebase'],
   },
   {
     title: 'Elevare',
@@ -28,6 +22,12 @@ const projects = [
     description:
       'Architect application to request data from APIs such as Facebook Graph, Google Analytics and Yelp Fusion.',
     tech: ['nodejs', 'react', 'mongo', 'graphql', 'd3'],
+  },
+  {
+    title: 'Patient Pop',
+    bg: 'linear-gradient(to right, #009245 0%, #FCEE21 100%)',
+    description: 'Design and engineer application to automate onboarding of new customers into AWS infrastructure.',
+    tech: ['php', 'awscm', 'laravel'],
   },
 ];
 
